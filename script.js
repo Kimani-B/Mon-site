88d27a66067f79f8e677275fe8894a06c63e0d58
Kimani-B <becksjosepha@yahoo.com>
2024-12-05 15:53:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Si vous avez besoin d'ajouter des fonctionnalités JavaScript supplémentaires, voici un exemple simple.
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Le site est prêt !");
+});
